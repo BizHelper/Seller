92ddb4f848c0030fc03625520b9fcecc8a36cf72
Phua Li Ting <phualiting8@gmail.com>
2022-07-04 17:46:55 +0800

    cover navigation bar on listing page when keyboard appears

--- lib/src/screens/listing.dart
+++ lib/src/screens/listing.dart
@@ -7,7 +7,10 @@ import 'package:seller_app/src/widgets/navigateBar.dart';
 class ListingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.blueGrey[50],
       appBar: AppBar(
         backgroundColor: Colors.cyan[900],
@@ -30,20 +33,23 @@ class ListingScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: ListingForm(),
-                ),
+      body: SizedBox(
+        height: screenHeight - keyboardHeight,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: ListingForm(),
+                  ),
+              ),
             ),
-          ),
-          NavigateBar(),
-        ],
+            NavigateBar(),
+          ],
+        ),
       ),
     );
   }
