c8fca6807a20026bd17e8eb45753b223160f7c0f
Phua Li Ting <phualiting8@gmail.com>
2022-07-23 22:54:39 +0800

    improved account page widgets

--- lib/src/screens/account.dart
+++ lib/src/screens/account.dart
@@ -15,7 +15,7 @@ class AccountScreen extends StatefulWidget {
 
 class _AccountScreenState extends State<AccountScreen> {
   String _sellerName = '';
-  var _image = '';
+  var _image = 'https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png';
 
   Future<String> getSellerName() async {
     final uid = AuthService().currentUser?.uid;
@@ -48,6 +48,7 @@ class _AccountScreenState extends State<AccountScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.blueGrey[50],
       appBar: AppBar(
         backgroundColor: Colors.cyan[900],
@@ -74,97 +75,101 @@ class _AccountScreenState extends State<AccountScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Column(
-            children: [
-              const SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                width: 200,
-                height: 200,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
-                  image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: Image.network(getImage()).image,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                getName(),
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(
-                height: 30.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
                 children: [
-                  ElevatedButton(
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(Colors.orange[600])),
-                    onPressed: () async {
-                      final uid = AuthService().currentUser?.uid;
-                      DocumentSnapshot ds = await FirebaseFirestore.instance.collection('sellers').doc(uid).get();
-                      String hasShop = ds.get('hasShop');
-                      String name = ds.get('Name');
-                      String address = '';
-                      String description = '';
-                      if (hasShop == 'true') {
-                        address = ds.get('Address');
-                        description = ds.get('Description');
-                      }
-                      Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) => ShopInfoScreen(
-                            hasShop: hasShop,
-                            name: name,
-                            address: address,
-                            description: description,
-                          )));
-                    },
-                    child: const Text(
-                      'Update Shop Info',
-                      style: TextStyle(color: Colors.black),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  Container(
+                    width: 200,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                      image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: Image.network(getImage()).image,
+                      ),
                     ),
                   ),
-                  ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.orange[600])),
-                    onPressed: () async {
-                      final uid = AuthService().currentUser?.uid;
-                      String sellerName = getName();
-                      DocumentReference dr = FirebaseFirestore.instance.collection('sellers').doc(uid);
-                      final XFile? pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
-                      if (pickedFile == null) {
-                        return;
-                      }
-                      final File image = (File(pickedFile.path));
-
-                      FirebaseStorage storage = FirebaseStorage.instance;
-                      Reference ref = storage.ref().child(pickedFile.path + DateTime.now().toString());
-                      await ref.putFile(image);
-                      String imageURL = await ref.getDownloadURL();
-                      dr.set({
-                        'Name': sellerName,
-                        'Profile Pic': imageURL
-                      });
-                      setState(() => _image = imageURL);
-                    },
-                    child: const Text(
-                      'Change Profile Picture',
-                      style: TextStyle(color: Colors.black),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    getName(),
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
+                  ),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(Colors.orange[600])),
+                        onPressed: () async {
+                          final uid = AuthService().currentUser?.uid;
+                          DocumentSnapshot ds = await FirebaseFirestore.instance.collection('sellers').doc(uid).get();
+                          String hasShop = ds.get('hasShop');
+                          String name = ds.get('Name');
+                          String address = '';
+                          String description = '';
+                          if (hasShop == 'true') {
+                            address = ds.get('Address');
+                            description = ds.get('Description');
+                          }
+                          Navigator.of(context).push(
+                              MaterialPageRoute(builder: (context) => ShopInfoScreen(
+                                hasShop: hasShop,
+                                name: name,
+                                address: address,
+                                description: description,
+                              )));
+                        },
+                        child: const Text(
+                          'Update Shop Info',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Colors.orange[600])),
+                        onPressed: () async {
+                          final uid = AuthService().currentUser?.uid;
+                          String sellerName = getName();
+                          DocumentReference dr = FirebaseFirestore.instance.collection('sellers').doc(uid);
+                          final XFile? pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+                          if (pickedFile == null) {
+                            return;
+                          }
+                          final File image = (File(pickedFile.path));
+
+                          FirebaseStorage storage = FirebaseStorage.instance;
+                          Reference ref = storage.ref().child(pickedFile.path + DateTime.now().toString());
+                          await ref.putFile(image);
+                          String imageURL = await ref.getDownloadURL();
+                          dr.set({
+                            'Name': sellerName,
+                            'Profile Pic': imageURL
+                          });
+                          setState(() => _image = imageURL);
+                        },
+                        child: const Text(
+                          'Change Profile Picture',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
           NavigateBar(),
         ],
